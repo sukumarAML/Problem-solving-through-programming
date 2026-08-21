45403df095adc8d8749da45f15e1032532baeee6
sukumarAML <147479940+sukumarAML@users.noreply.github.com>
2026-08-21 10:56:28 +0530

    Change addition to subtraction in integer program

--- Unit1/subtraction of two integers.c
+++ Unit1/subtraction of two integers.c
@@ -1,12 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
-{ int a,b,sum;
-  printf("Addition of two numbers:\n");
-  printf("Enter the first number:");
-  scanf("%d",&a);
-  printf("Enter the second number:");
-  scanf("%d",&b);
-  sum = a-b;
-  printf("the sum of %d and %d is %d",a,b,sum);
-  return 0;
+{
+    int a, b, difference;
+
+    printf("Subtraction of two integers\n");
+
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+
+    difference = a - b;
+
+    printf("The difference of %d and %d is %d", a, b, difference);
+
+    return 0;
 }
