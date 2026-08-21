e7f665a719310ba2a3d7875c1c1a84479f5d5585
Your Name <your.email@example.com>
2026-08-21 14:26:29 +0530

    safer

---
+++ Unit1/character_op/string_index_print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char name[] = "SRMIST";
+    printf("%s\n", name);
+    printf("%c\n", name[0]);
+    printf("%c\n", name[1]);
+    printf("%c\n", name[2]);
+    printf("%c\n", name[3]);
+    printf("%c\n", name[4]);
+    printf("%c\n", name[5]);
+    return 0;
+}
