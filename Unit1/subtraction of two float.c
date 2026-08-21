6389482e9fa5b2cefdcf8dd8131415f84b432981
sukumarAML <147479940+sukumarAML@users.noreply.github.com>
2026-08-21 10:55:23 +0530

    Implement subtraction of two float numbers
    Fix the code to perform subtraction of two float numbers instead of addition.

---
+++ Unit1/subtraction of two float.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ float a,b,sum;
+  printf("Addition of two numbers:\n");
+  printf("Enter the first number:");
+  scanf("%d",&a);
+  printf("Enter the second number:");
+  scanf("%d",&b);
+  sum = a-b;
+  printf("the sum of %d and %d is %d",a,b,sum);
+  return 0;
+}
