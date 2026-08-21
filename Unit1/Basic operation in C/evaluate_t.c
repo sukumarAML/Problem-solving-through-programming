ee45fb16b1183413de2fc610f15f6f41c91dda47
Your Name <your.email@example.com>
2026-08-21 14:51:44 +0530

    safer

---
+++ Unit1/Basic operation in C/evaluate_t.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a = 15;
+    int b = 4;
+    float y;
+    int s = a / b;
+    x = a % b;
+    y = 15.0/4.0;
+    int p = 6*2-4;
+    int q =(6*2)-(10%3);
+
+    printf("The sum of %d and %d is %d, %d, %.2f, %d, %d\n", a, b, s, x , y, p, q);
+
+    return 0;
+}   
