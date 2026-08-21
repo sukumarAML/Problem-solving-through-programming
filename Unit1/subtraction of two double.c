e72ed9fd2d7960a2a58a7eaeefc554fb5a91e3d3
sukumarAML <147479940+sukumarAML@users.noreply.github.com>
2026-08-21 10:54:01 +0530

    Create subtraction of two double.c

---
+++ Unit1/subtraction of two double.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    double a, b, sum;
+
+    printf("Enter two double values: ");
+    scanf("%lf %lf", &a, &b);
+
+    sum = a - b;
+
+    printf("Sum = %.2lf\n", sum);
+
+    return 0;
+}
