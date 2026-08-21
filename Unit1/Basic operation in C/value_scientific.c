d0ca52eab58bdbd3a4e145a4684da27cc5f1e439
Your Name <your.email@example.com>
2026-08-21 14:43:13 +0530

    safer

---
+++ Unit1/Basic operation in C/value_scientific.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    double a = 1300;
+    double b = 123.45;
+    double c = 0.00426;
+
+    printf("%e\n", a);
+    printf("%e\n", b);
+    printf("%e\n", c);
+
+    return 0;
+}
