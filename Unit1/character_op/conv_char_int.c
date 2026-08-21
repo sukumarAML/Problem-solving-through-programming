86ccf3d4fea6793e55a2f5b044efd54c76cd1af9
Your Name <your.email@example.com>
2026-08-21 13:41:25 +0530

    safe

---
+++ Unit1/character_op/conv_char_int.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+char ch = 'A';
+int x = ch;
+printf("The character is: %c\n", ch);
+printf("The ASCII value is: %d\n", x);
+char st = '8';
+int y = st;
+printf("The character is: %c\n", st);
+printf("The ASCII value is: %d\n", y);
+return 0;
+}
