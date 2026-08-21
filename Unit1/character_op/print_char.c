89c798ce3f278e1b3302ab674c732a695fb1e351
Your Name <your.email@example.com>
2026-08-21 13:33:06 +0530

    safe

--- Unit1/character_op/print_char.c
+++ Unit1/character_op/print_char.c
@@ -5,5 +5,7 @@ int main()
     int x = ch;
     printf("The character is: %c\n", ch);
     printf("The ASCII value is: %d\n", x);
+    int n =  ch - '0';
+    printf("The numeric value is: %d\n", n);
     return 0;
 }
