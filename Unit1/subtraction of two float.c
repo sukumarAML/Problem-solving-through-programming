ada13b0c80d3aa95483c407c4b4a332f4c44cb61
sukumarAML <147479940+sukumarAML@users.noreply.github.com>
2026-08-21 10:56:57 +0530

    Modify program for subtraction of two float numbers

--- Unit1/subtraction of two float.c
+++ Unit1/subtraction of two float.c
@@ -1,12 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
-{ float a,b,sum;
-  printf("Addition of two numbers:\n");
-  printf("Enter the first number:");
-  scanf("%d",&a);
-  printf("Enter the second number:");
-  scanf("%d",&b);
-  sum = a-b;
-  printf("the sum of %d and %d is %d",a,b,sum);
-  return 0;
+{
+    float a, b, difference;
+
+    printf("Subtraction of two float numbers\n");
+
+    printf("Enter the first number: ");
+    scanf("%f", &a);
+
+    printf("Enter the second number: ");
+    scanf("%f", &b);
+
+    difference = a - b;
+
+    printf("The difference of %.2f and %.2f is %.2f", a, b, difference);
+
+    return 0;
 }
