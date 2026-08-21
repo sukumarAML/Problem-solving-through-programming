6c3be0d91f5b964a471d9fe129447e17b88dca47
sukumarAML <147479940+sukumarAML@users.noreply.github.com>
2026-08-21 10:37:57 +0530

    Create addition.c

---
+++ Unit1/addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  printf("Addition of two numbers:\n");
+  int a,b,sum;
+  scanf("Enter the first number:%d", a);
+  scanf("Enter the second number:%d", b);
+  sum = a+b;
+  printf("the sum of %d and %d is %d",a,b,sum);
+}
