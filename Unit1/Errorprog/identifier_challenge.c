89fe194b27eef7a249590c0637897237420b37e7
Your Name <your.email@example.com>
2026-08-24 22:28:32 +0530

    safe

---
+++ Unit1/Errorprog/identifier_challenge.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+int main()
+{
+    int student1 = 20;
+    int 2student = 30;
+
+    printf("%d %d", student1, 2student);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+    int total marks = 500;
+
+    printf("%d", total marks);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+    int student-name = 25;
+
+    printf("%d", student-name);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+    int return = 10;
+
+    printf("%d", return);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+    float student@mark = 87.5;
+
+    printf("%f", student@mark);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+    int _total = 100;
+
+    printf("%d", _total);
+
+    return 0;
+}
+
+/*
+A valid C identifier:
+
+Can contain letters (A-Z, a-z).
+Can contain digits (0-9), but cannot start with a digit.
+Can contain the underscore _.
+Cannot contain spaces.
+Cannot contain special characters such as @, #, $, -, ..
+Cannot be a C keyword such as int, float, if, else, return, while.
+C identifiers are case-sensitive: marks, Marks, and MARKS are different identifiers.
+
+*/
