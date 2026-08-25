cbbf2401d8907957c354c83e7a20146599617fee
Your Name <your.email@example.com>
2026-08-25 08:55:09 +0530

    sasa

--- Unit1/Errorprog/error_identifiers.c
+++ Unit1/Errorprog/error_identifiers.c
@@ -7,4 +7,5 @@ int main()
     printf("%d", 2marks);
 
     return 0;
-}The code you provided has a syntax error because variable names in C cannot start with a digit. In this case, `2marks` is an invalid identifier. You should rename the variable to start with a letter or an underscore. Here’s a corrected version of your code:
+}
+//The code you provided has a syntax error because variable names in C cannot start with a digit. In this case, `2marks` is an invalid identifier. You should rename the variable to start with a letter or an underscore. Here’s a corrected version of your code:
