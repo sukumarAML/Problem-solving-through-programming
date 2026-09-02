191753036c50fd12a41e5a5f14a706a856d00a4f
Your Name <your.email@example.com>
2026-09-02 14:57:23 +0530

    safe

---
+++ Unit1/primeevodd.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int value;
+    scanf("%d", &value);
+    int max = (if((Value % 2 == 0) || (Value % 3 == 0) || (Value % 5 == 0) || (Value % 7 == 0)))?(value%2==0):value;
+    printf("%d",max);
+    return 0;
+}
