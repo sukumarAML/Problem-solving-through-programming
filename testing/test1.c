4a53c604e8eb2f9bc2b9434769f408e6f1435f34
Your Name <your.email@example.com>
2026-09-07 14:14:39 +0530

    safe

---
+++ testing/test1.c
@@ -0,0 +1,32 @@
+// #include <stdio.h>
+// int main() {
+// int a = 10;
+// float b = 3.5;
+// double c = 2.25;
+
+// double result = a + b * c;
+// printf("result: %f\n", result);
+// printf("Variable result is of type: %lu\n", sizeof(result));
+// return 0;
+// }
+
+/* printf("Variable a is of type: %s\n", get_type(a));
+printf("Variable b is of type: %s\n", get_type(b));
+printf("Variable c is of type: %s\n", get_type(c));
+printf("Variable d is of type: %s\n", get_type(d)); */
+
+
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    float height;
+    char grade;
+
+    scanf("%f", &age);
+    scanf("%d", &height);
+    scanf("%s", &grade);
+
+    return 0;
+}
